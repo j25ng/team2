6fa008689940827c2c92bcc080c6cc99f542da55
j25ng <jsan2861@gmail.com>
2025-08-18 10:07:29 +0900

    fix cors

--- an/server/server.js
+++ an/server/server.js
@@ -35,7 +35,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: [
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+      "https://team2-tau.vercel.app",
+    ],
     credentials: true,
   })
 );
